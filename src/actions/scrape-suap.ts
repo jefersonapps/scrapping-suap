588591d05476a11c96d320c7f6dc3f0b85c320f3
jefersonapps <jefersonplaynunes@gmail.com>
2024-05-19 01:28:23 -0300

    fix:server-bug

--- src/actions/scrape-suap.ts
+++ src/actions/scrape-suap.ts
@@ -1,7 +1,8 @@
 "use server";
 
+import chrome from "chrome-aws-lambda";
 import { revalidatePath } from "next/cache";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 
 export interface UserData {
   nome: string;
@@ -30,7 +31,22 @@ export async function scrapeSuap(
   userPassword: string
 ): Promise<UserData | null> {
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const options = process.env.AWS_REGION
+      ? {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+      : {
+          args: [],
+          executablePath:
+            process.platform === "win32"
+              ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+              : process.platform === "linux"
+              ? "/usr/bin/google-chrome"
+              : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+        };
+    const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle0", timeout: 120000 });
 
